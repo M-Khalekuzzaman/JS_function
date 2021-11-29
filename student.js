f07d6d1288d099ca59ff88c3eb4d62bac98e6716
Kaochar <94604055+M-Khalekuzzaman@users.noreply.github.com>
2021-11-29 10:02:45 +0600

    Add files via upload

---
+++ student.js
@@ -0,0 +1,20 @@
+const getName = () =>{
+    return "Khalekuzzaman";
+}
+const getAge = () =>{
+    return "25";
+}
+const getRoll = () =>{
+    return "1910979158";
+}
+const cgpa =  3.92;
+// exports.getName = getName;
+// exports.getAge = getAge;
+// exports.getRoll = getRoll;
+// exports.result = cgpa;
+module.exports = {
+    getName,
+    getAge,
+    getRoll,
+    cgpa
+}
